ab3fb32f5df603caf3cf6ab346ee29ad5b5c1b9a
Aaron Krajeski <ak@aaronk.is>
2018-05-17 17:29:50 -0400

    Fix audio problem in firefox

--- js/Transport.js
+++ js/Transport.js
@@ -7,6 +7,7 @@ function Transport(options) {
   this.startPosition = Constants.NUM_STEPS - 1;
   this.previousPosition = Constants.NUM_STEPS - 1;
   this.position = this.previousPosition;
+  this.distanceFromBeat = 0;
   this.swing = (options.swing !== undefined) ? 0 : options.swing;
 
   this.start = function() {
@@ -30,12 +31,14 @@ Transport.prototype.update = function(beatCallback) {
     return;
   }
   var secondsSinceStart = (performance.now() - this.startTime) / 1000;
-  var beats = Controls.TEMPO * secondsSinceStart / 60;
-  this.position = beats * (1/4 / Constants.STEP_VALUE) + this.startPosition;
-  if (Math.floor(this.position) % 2 === 1) {
-    this.position += this.swing;
+  var numBeatsPassed = Controls.TEMPO * secondsSinceStart / 60;
+  var floatPosition = numBeatsPassed * (1/4 / Constants.STEP_VALUE) + this.startPosition;
+  if (Math.floor(floatPosition) % 2 === 1) {
+    floatPosition += this.swing;
   }
-  this.position = Math.floor(this.position % Constants.NUM_STEPS);
+  var normalizedFloatPosition = floatPosition % Constants.NUM_STEPS;
+  this.position = Math.floor(normalizedFloatPosition);
+  this.distanceFromBeat = normalizedFloatPosition - this.position;
 
   if (this.position !== this.previousPosition) {
     this.previousPosition = this.position;
