72b2ee93aa6c6d99e012d677a29e3050971a5d2e
Aaron Krajeski <ak@aaronk.is>
2018-06-21 17:20:33 -0400

    Make word list for hasher

--- hasher.py
+++ hasher.py
@@ -1,5 +1,5 @@
 import hashlib
-NUM_WORDS = 3
+import argparse
 
 def max_int_from_string(string, max_val, current_value=''):
   if len(string) == 0:
@@ -9,19 +9,29 @@ def max_int_from_string(string, max_val, current_value=''):
 
   return max_int_from_string(string[1:], max_val, current_value=current_value + string[0])
 
-file_path = "/usr/share/dict/words"
-words = []
-with open(file_path, 'r') as infile:
-  for line in infile:
-    words.append(line.rstrip("\n"))
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(dest='input_word', action='store')
+  parser.add_argument('-n', '--num-words', dest='num_words', type=int, default=3)
+  args = parser.parse_args()
 
-num_words = len(words)
-hex_digest = hashlib.sha1(b'Hey guys').hexdigest()
-int_digest = str(int(hex_digest, 16))
-hash_words = []
+  dict_file = "word_list.txt"
+  words = []
+  with open(dict_file, 'r') as infile:
+    for line in infile:
+      words.append(line.rstrip("\n"))
 
-while len(hash_words) < NUM_WORDS:
-  word_num, int_digest = max_int_from_string(int_digest, num_words)
-  hash_words.append(words[word_num])
+  num_words = len(words)
+  hex_digest = hashlib.sha1(str.encode(args.input_word)).hexdigest()
+  int_digest = str(int(hex_digest, 16))
+  hash_words = []
 
-print(hash_words)
+  while len(hash_words) < args.num_words:
+    word_num, int_digest = max_int_from_string(int_digest, num_words)
+    hash_words.append(words[word_num])
+
+  result = '-'.join(hash_words)
+  print(result)
+
+if __name__ == '__main__':
+  main()
