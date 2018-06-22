ef2181f1c7115e8c1209803d7d2dffd9bcfabfae
Aaron Krajeski <ak@aaronk.is>
2018-06-22 09:31:44 -0400

    Create tester for hasher, too many collisions

--- hasher.py
+++ hasher.py
@@ -2,40 +2,27 @@ import hashlib
 import argparse
 import textwrap
 
-def max_int_from_string(string, max_val, current_value=''):
-  if len(string) == 0:
-    return int(current_value), string
-  if int(current_value + string[-1]) > max_val:
-    return int(current_value), string
-
-  return max_int_from_string(string[:-1], max_val, current_value=current_value + string[-1])
-
-def hash_to_words(input_word, num_words=3):
-  dict_file = "word_list.txt"
+def load_dictionary(file_path):
   dictionary_words = []
-  with open(dict_file, 'r') as infile:
+  with open(file_path, 'r') as infile:
     for line in infile:
       dictionary_words.append(line.rstrip("\n"))
+  return dictionary_words
 
+def hash_to_words(input_word, num_words=3, dictionary=None):
   hex_digest = hashlib.sha1(str.encode(input_word)).hexdigest()
   int_digest = str(int(hex_digest, 16))
   indeces = textwrap.wrap(int_digest, len(int_digest) // num_words + 1)
-  hash_words = [dictionary_words[int(x) % len(dictionary_words)] for x in indeces]
+  indeces = [int(x) % len(dictionary) for x in indeces]
+  hash_words = [dictionary[x] for x in indeces]
 
-  # while len(hash_words) < num_words:
-  #   word_num, int_digest = max_int_from_string(int_digest, len(dictionary_words) - 1)
-  #   try:
-  #     hash_words.append(dictionary_words[word_num])
-  #   except:
-  #     print(word_num)
-
-  # import pdb; pdb.set_trace()
   result = '-'.join(hash_words)
-  return result
+  return result, indeces
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument(dest='input_word', action='store')
   parser.add_argument('-n', '--num-words', dest='num_words', type=int, default=3)
   args = parser.parse_args()
-  hash_to_words(args.input_word, args.num_words)
+  result = hash_to_words(args.input_word, num_words=args.num_words, dictionary=load_dictionary("word_list.txt"))
+  print(result)
